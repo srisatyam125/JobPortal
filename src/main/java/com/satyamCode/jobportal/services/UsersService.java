e0233fb856de51216afa9d6058229b5bf48d15d0
srisatyam125 <srisatyam0526@gmail.com>
2026-08-15 17:28:38 +0530

    custom authentication success

--- src/main/java/com/satyamCode/jobportal/services/UsersService.java
+++ src/main/java/com/satyamCode/jobportal/services/UsersService.java
@@ -7,6 +7,7 @@ import com.satyamCode.jobportal.repository.JobSeekerProfileRepository;
 import com.satyamCode.jobportal.repository.RecruiterProfileRepository;
 import com.satyamCode.jobportal.repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -17,16 +18,19 @@ public class UsersService {
     private final UsersRepository usersRepository;
     private final RecruiterProfileRepository recruiterProfileRepository;
     private final JobSeekerProfileRepository jobSeekerProfileRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UsersService(UsersRepository usersRepository, RecruiterProfileRepository recruiterProfileRepository, JobSeekerProfileRepository jobSeekerProfileRepository) {
+    public UsersService(UsersRepository usersRepository, RecruiterProfileRepository recruiterProfileRepository, JobSeekerProfileRepository jobSeekerProfileRepository, PasswordEncoder passwordEncoder) {
         this.usersRepository = usersRepository;
         this.recruiterProfileRepository = recruiterProfileRepository;
         this.jobSeekerProfileRepository = jobSeekerProfileRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public Users addNew(Users users){
         users.setActive(true);
         users.setRegistrationDate(new Date(System.currentTimeMillis()));
+        users.setPassword(passwordEncoder.encode(users.getPassword()));
         Users savedUser = usersRepository.save(users);
         int userTypeId = users.getUserTypeId().getUserTypeId();
         if(userTypeId == 1){
