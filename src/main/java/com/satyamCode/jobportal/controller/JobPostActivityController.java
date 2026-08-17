d9c983b5c29b5194ea74d63cac0b00f133c78fb2
srisatyam125 <srisatyam0526@gmail.com>
2026-08-17 18:22:56 +0530

    Display and retrieve Jobs posted by a recruiter

--- src/main/java/com/satyamCode/jobportal/controller/JobPostActivityController.java
+++ src/main/java/com/satyamCode/jobportal/controller/JobPostActivityController.java
@@ -1,12 +1,15 @@
 package com.satyamCode.jobportal.controller;
 
 import com.satyamCode.jobportal.entity.JobPostActivity;
+import com.satyamCode.jobportal.entity.RecruiterJobsDto;
+import com.satyamCode.jobportal.entity.RecruiterProfile;
 import com.satyamCode.jobportal.entity.Users;
 import com.satyamCode.jobportal.services.JobPostActivityService;
 import com.satyamCode.jobportal.services.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Date;
+import java.util.List;
 
 @Controller
 public class JobPostActivityController {
@@ -34,6 +38,11 @@ public class JobPostActivityController {
         if(!(authentication instanceof AnonymousAuthenticationToken)){
             String currentUsername  = authentication.getName();
             model.addAttribute("username",currentUsername);
+            if(authentication.getAuthorities().contains(new SimpleGrantedAuthority("Recruiter"))){
+                List<RecruiterJobsDto> recruiterJobs = jobPostActivityService.getRecruiterJobs(((RecruiterProfile)
+                        currentUserProfile).getUserAccountId());
+                model.addAttribute("jobPost", recruiterJobs);
+            }
         }
         model.addAttribute("user",currentUserProfile);
         return "dashboard";
---
+++ src/main/java/com/satyamCode/jobportal/entity/IRecruiterJobs.java
@@ -0,0 +1,22 @@
+package com.satyamCode.jobportal.entity;
+
+public interface IRecruiterJobs {
+
+    Long getTotalCandidates();
+
+    int getJob_post_id();
+
+    String getJob_title();
+
+    int getLocationId();
+
+    String getCity();
+
+    String getState();
+
+    String getCountry();
+
+    int getCompanyId();
+
+    String getName();
+}
